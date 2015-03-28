e376f09d7ab1450b62789697677a0a5cb8ac7abd
Yaroslawa Chernopischenko <chernopyshenko@gmail.com>
2015-03-28 15:05:02 +0200

    Add a new common title for all the pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,7 +13,12 @@ describe "Static pages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} | Home")
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
@@ -26,7 +31,12 @@ describe "Static pages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("#{base_title} | Help")
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/help'
+      expect(page).not_to have_title('| Help')
     end
   end
 
@@ -39,7 +49,12 @@ describe "Static pages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("#{base_title} | About Us")
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/about'
+      expect(page).not_to have_title('| About Us')
     end
   end
 
@@ -50,7 +65,11 @@ describe "Static pages" do
   	end
   	it "should have the title 'Contact'" do
   	  visit '/static_pages/contact'
-  	  expect(page).to have_title("#{base_title} | Contact")
+  	  expect(page).to have_title("#{base_title}")
   	end
+  	it "should not have a custom page title" do
+      visit '/static_pages/contact'
+      expect(page).not_to have_title('| Contact')
+    end
   end
 end
